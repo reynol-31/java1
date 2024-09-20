3917865cde8ca54a1d48196f5c017b3239d55f87
reynol-31 <reynol.23cs119@sode-edu.in>
2024-09-20 23:15:18 +0530

    Update TestEmployee.java
    included second employee with 10000 salary

--- TestEmployee.java
+++ TestEmployee.java
@@ -14,14 +14,27 @@ class Employee{
     else{
       System.out.println("Salary: "+salary);}}
   public void getEmpDetails(){
-    System.out.println(name + " " + id + " "+ salary);}}
+    System.out.println(name + " " + id + " "+ salary);}
+  public void compare(Employee emp1,Employee emp2){
+    if (emp1.salary>emp2.salary) {
+      System.out.println(name + " "+ id + " gets more salary: "+salary );}
+    else {
+      System.out.println(emp2.name + " "+ emp2.id + " gets more salary: "+emp2.salary );      
+    }
+    }
+}
       
 public class TestEmployee{
   public static void main(String[] args){
     Employee emp1=new Employee(1,"ben",5000);
+    Employee emp2=new Employee(2,"sam", 10000);
     emp1.getEmpDetails();
+    emp2.getEmpDetails();
     emp1.raiseSalary(10);
+    emp2.raiseSalary(10);
     emp1.getEmpDetails();
+    emp2.getEmpDetails();
+    emp1.compare(emp1, emp2);
   
   }
 }  
