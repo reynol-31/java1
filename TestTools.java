eb68f86f320888e844ed15050da84c82c165b16d
reynol-31 <reynol.23cs119@sode-edu.in>
2024-11-14 10:25:54 +0530

    Add files via upload

---
+++ TestTools.java
@@ -0,0 +1,12 @@
+package pkg1;
+public class TestTools {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.numbers(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.mult3(10);
+        t1.mult4(10);
+        t1.mult5(10);
+    }
+}
---
+++ Tools.java
@@ -0,0 +1,39 @@
+package pkg1;
+public class Tools {
+    public void numbers(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void even(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i*2+" ");
+        }
+        System.out.println();
+    }
+    public void odd(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i*2+1+" ");
+        }
+        System.out.println();
+    }
+    public void mult3(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i*3+" ");
+        }
+        System.out.println();
+    }
+    public void mult4(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i*4+" ");
+        }
+        System.out.println();
+    }
+    public void mult5(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i*5+" ");
+        }
+        System.out.println();
+    }    
+}
