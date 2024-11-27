695926e39d8a32265c90d640cee6e013bc3c9d0b
reynol-31 <reynol.23cs119@sode-edu.in>
2024-11-28 00:29:36 +0530

    Add files via upload

---
+++ TestMainPackage.java
@@ -0,0 +1,7 @@
+import mypack.TestPackage;
+public class TestMainPackage {
+    public static void main(String[] args) {
+        TestPackage pack=new TestPackage();
+        pack.display();
+    }
+}
